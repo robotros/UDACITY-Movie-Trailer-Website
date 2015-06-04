ec777539c83dafa92397d9acac42dfa4a9d9f454
Aron Roberts <robotros1@msn.com>
2015-06-04 11:48:23 -0700

    PEP8 Requirements
    Updated PEP8 Requirements

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,13 +1,18 @@
-"""Movie Trailer website
+""" Movie Trailer website
 
 UDACITY Full stack Developer Project 1
 Provided by UDACITY slightly modified
 
-Generates and opens HTML for movie trailer website using an array of movie objects
+Generates and opens HTML for movie trailer website
+using an array of movie objects
 
 modifiedBy: Aron Roberts
-Date: 6/3/2015
+Date: 6/4/2015
 filename: fresh_tomatoes.py
+
+Last Update: PEP8 Requirements
+Some lines exceed the length requirement
+
 """
 
 import webbrowser
@@ -17,17 +22,19 @@ import re
 # Styles and scripting for the page
 main_page_head = '''
 <head>
-    <meta charset="utf-8">
-    <!--Modified TITLE-->
-    <title>Star Wars Trailers</title>
-    
+ <meta charset="utf-8">
+ <!--Modified TITLE-->
+ <title>Star Wars Trailers</title>
 
-    <!-- Bootstrap 3 -->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
-    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-    <style type="text/css" media="screen">
+ <!-- Bootstrap 3 -->
+ <link rel="stylesheet"
+  href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+ <link rel="stylesheet"
+  href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+ <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+ <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js">
+ </script>
+ <style type="text/css" media="screen">
         body {
             padding-top: 80px;
         }
@@ -70,16 +77,21 @@ main_page_head = '''
     </style>
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
-            // Remove the src so the player itself gets removed, as this is the only
-            // reliable way to ensure the video stops playing in IE
+        $(document).on('click',
+                       '.hanging-close,
+                       .modal-backdrop,
+                       .modal',
+                       function (event) {
+            // Remove the src so the player itself gets removed,
+            // Only reliable way to ensure the video stops playing in IE
             $("#trailer-video-container").empty();
         });
         // Start playing the video whenever the trailer modal is opened
         $(document).on('click', '.movie-tile', function (event) {
             var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
             var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+            $("#trailer-video-container").empty().append($("<iframe></iframe>",
+            {
               'id': 'trailer-video',
               'type': 'text-html',
               'src': sourceUrl,
@@ -105,7 +117,10 @@ main_page_content = '''
     <div class="modal" id="trailer">
       <div class="modal-dialog">
         <div class="modal-content">
-          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
+          <a href="#"
+             class="hanging-close"
+             data-dismiss="modal"
+             aria-hidden="true">
             <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
           </a>
           <div class="scale-media" id="trailer-video-container">
@@ -113,7 +128,7 @@ main_page_content = '''
         </div>
       </div>
     </div>
-    
+
     <!-- Main Page Content -->
     <div class="container">
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -134,45 +149,53 @@ main_page_content = '''
 
 # A single movie entry html template
 movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+<div class="col-md-6 col-lg-4 movie-tile text-center"
+     data-trailer-youtube-id="{trailer_youtube_id}"
+     data-toggle="modal"
+     data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
     <p> Rating : {rating} </br> {storyline}</p>
 </div>
 '''
 
+
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
         # Extract the youtube ID from the url
-        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+        youtube_id_match = re.search(r'(?<=v=)[^&#]+',
+                                     movie.trailer_youtube_url)
+        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+',
+                                                         movie.trailer_youtube_url)
         trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
 
-        # Append the tile for the movie with its content filled in
-        # Added storyline and rating
-        content += movie_tile_content.format(
-            movie_title=movie.title,
-            poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id,
-            storyline = movie.storyline,
-            rating = movie.rating
-        )
+    # Append the tile for the movie with its content filled in
+    # Added storyline and rating
+    content += movie_tile_content.format(
+      movie_title=movie.title,
+      poster_image_url=movie.poster_image_url,
+      trailer_youtube_id=trailer_youtube_id,
+      storyline=movie.storyline,
+      rating=movie.rating)
+
     return content
 
+
 def open_movies_page(movies):
-  # Create or overwrite the output file
-  # changed output name
-  output_file = open('project1.html', 'w')
+    # Create or overwrite the output file
+    # changed output name
+    output_file = open('project1.html', 'w')
 
-  # Replace the placeholder for the movie tiles with the actual dynamically generated content
-  rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
+    # Replace placeholder movie info with dynamically generated content
+    rendered_content = main_page_content.format(
+        movie_tiles=create_movie_tiles_content(movies))
 
-  # Output the file
-  output_file.write(main_page_head + rendered_content)
-  output_file.close()
+    # Output the file
+    output_file.write(main_page_head + rendered_content)
+    output_file.close()
 
-  # open the output file in the browser
-  url = os.path.abspath(output_file.name)
-  webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
+    # open the output file in the browser
+    url = os.path.abspath(output_file.name)
+    webbrowser.open('file://' + url, new=2)  # open in a new tab, if possible
