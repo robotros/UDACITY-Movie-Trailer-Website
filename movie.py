1f853ba80b1c11c9f2c672712676b28cd1b85be2
Aron Roberts <robotros1@msn.com>
2015-06-04 10:52:34 -0700

    PEP8 Requirements
    update file to follow PEP8 Requirements

--- movie.py
+++ movie.py
@@ -1,14 +1,17 @@
-"""Movie module
+""" Movie module
 
 This module contains the movie class for use in
 the UDACITY Full Stack Web Developer Project 1
 
 Author: Aron Roberts
-Version: 1.02
-Date: 6/3/2015
+Version: 1.03
+Date: 6/4/2015
 filename: movie.py
 
+Last Update: update file to follow PEP8 Requirements
+
 """
+
 
 class Movie():
     """ This class provides a way to store related information for a movie
@@ -17,11 +20,14 @@ class Movie():
         VALID_RATINGS (list of str): List of Valid MPAA ratings
 
     """
-    
+
     VALID_RATINGS = ["G", "PG", "PG-13", "R", "NR"]
 
-    
-    def __init__(self, movie_title, movie_storyline,movie_poster, movie_trailer, movie_rating="NR") :
+    def __init__(self, movie_title,
+                 movie_storyline,
+                 movie_poster,
+                 movie_trailer,
+                 movie_rating="NR"):
         """ Movie Class Constructor
 
         Args:
@@ -29,20 +35,18 @@ class Movie():
             movie_storyline(str): Storyline of movie
             movie_poster(str): URL for movie poster
             movie_trailer(str): URL for movie trailer
-            movie_rating (str, optional): MPAA rating NR if invalid or not provided    
-   
+            movie_rating (str, optional): MPAA rating, default NR
+
         """
 
-        #initialize Attributes
+        # initialize Attributes
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster
         self.trailer_youtube_url = movie_trailer
 
-        #check if rating is valid
+        # check if rating is valid
         if movie_rating in self.VALID_RATINGS:
             self.rating = movie_rating
-        else :
+        else:
             self.rating = "NR"
-        
-    
